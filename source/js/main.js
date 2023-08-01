14ec90d8125c894219ec0ca78c7077f3cd9ab5be
Sergey <serbog230797@gmail.com>
2023-08-02 01:13:31 +0300

    video

--- source/js/main.js
+++ source/js/main.js
@@ -21,7 +21,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initVideos();
+    document.querySelectorAll('[data-video]').forEach(initVideos);
     initTabs();
     initSwiperLast();
     initAccordions();
